9ff9c132bc520ff3c3036b50aa1c95c720c5253b
prathamewp <133904646+prathamewp@users.noreply.github.com>
2023-07-17 12:43:54 +0530

    Add files via upload

---
+++ restaurant-website/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './AboutUs.css'; 
+import LottiePlayer from '@lottiefiles/lottie-player';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>About Us</h1>
+      </header>
+      <div className="container">
+      
+        <h1>Welcome to Jay Bharat Restaurant</h1>
+        <div className="imag"></div>
+        <p>
+        Jay Bharat Restaurant is a place where delicious food meets excellent service. We are committed to providing our customers with an unforgettable dining experience.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
